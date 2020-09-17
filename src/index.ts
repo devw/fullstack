3b466a31c1b595658e9d4aab9b4b145154884a95
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-17 22:42:01 +0200

    Creating a new migration. $> npx mikro-orm migration:create

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(this);
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    await MikroORM.init(microConfig);
+};
+
+main().catch((err) => {
+    console.log(err);
+});
