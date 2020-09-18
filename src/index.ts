03c37370ad1644ba5ce0f10b208aa00737f5629b
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-18 11:39:19 +0200

    configuring the app to start a server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    // await orm.getMigrator().up();
-    const post = orm.em.create(Post, { title: "My 6th post" });
-    await orm.em.persistAndFlush(post);
+    await orm.getMigrator().up();
+    const app = express();
+    // const post = orm.em.create(Post, { title: "My 6th post" });
+    // await orm.em.persistAndFlush(post);
     const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    app.get("/", (_, res) => res.send(posts));
+    app.listen(4000, () =>
+        console.log("Server started at http://localhost:4000")
+    );
 };
 
 main().catch((err) => {
