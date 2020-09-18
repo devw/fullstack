d167cc4992bf74f1ce11f34f935af84a2b932e93
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-18 12:11:25 +0200

    implementing apollo-server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { NumberResolver } from "./resolvers/number";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -9,6 +12,15 @@ const main = async () => {
     const app = express();
     // const post = orm.em.create(Post, { title: "My 6th post" });
     // await orm.em.persistAndFlush(post);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [NumberResolver],
+            validate: false,
+        }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+
     const posts = await orm.em.find(Post, {});
     app.get("/", (_, res) => res.send(posts));
     app.listen(4000, () =>
